c8993c58c8fa4579577675f11a9a8eca69aa3a93
Ahmed AL-Alwani <ahmedalalwani477@gmail.com>
2025-07-17 10:57:04 +0300

    first upload

---
+++ Login.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace InventoryManagement
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btnlog = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(213, 64);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(415, 34);
+            this.txtname.TabIndex = 2;
+            this.txtname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtname.TextChanged += new System.EventHandler(this.txtname_TextChanged);
+            this.txtname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtname_KeyPress);
+            // 
+            // txtpass
+            // 
+            this.txtpass.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.Location = new System.Drawing.Point(213, 136);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.ReadOnly = true;
+            this.txtpass.Size = new System.Drawing.Size(385, 34);
+            this.txtpass.TabIndex = 3;
+            this.txtpass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtpass.TextChanged += new System.EventHandler(this.txtpass_TextChanged);
+            this.txtpass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtpass_KeyPress);
+            // 
+            // btnlog
+            // 
+            this.btnlog.Enabled = false;
+            this.btnlog.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlog.Location = new System.Drawing.Point(53, 203);
+            this.btnlog.Name = "btnlog";
+            this.btnlog.Size = new System.Drawing.Size(222, 57);
+            this.btnlog.TabIndex = 4;
+            this.btnlog.Text = "Login";
+            this.btnlog.UseVisualStyleBackColor = true;
+            this.btnlog.Click += new System.EventHandler(this.btnlog_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.Location = new System.Drawing.Point(406, 203);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(222, 57);
+            this.btnexit.TabIndex = 5;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(610, 146);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 272);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnlog);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Login_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btnlog;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,42 @@
+﻿using InventoryManagement.UserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagement
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnhome_Click(object sender, EventArgs e)
+        {
+            panelhome.Controls.Clear();
+            Home_UC h = new Home_UC();
+            h.Dock = DockStyle.Fill;
+            panelhome.Controls.Add(h);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            btnhome.Enabled = false;
+            btnproducts.Enabled = false;
+            btnsuppliers.Enabled = false;
+            btncustomers.Enabled = false;
+            btntransactions.Enabled = false;
+            btnreports.Enabled = false;
+            this.Show();
+            Login log = new Login(this);
+            log.ShowDialog();
+        }
+    }
+}
